9569f19f4fbf8b148b00ac53880937b7ded638aa
Oleh Honchar <ooollleeeg@gmail.com>
2023-03-17 07:48:54 +0200

    change Loader

--- src/shared/components/Loader/Loader.jsx
+++ src/shared/components/Loader/Loader.jsx
@@ -1,16 +1,18 @@
-import { ColorRing } from 'react-loader-spinner';
+import { RotatingLines } from 'react-loader-spinner';
+
+import styles from './loader.module.scss';
 
 export const Loader = () => {
   return (
-    <ColorRing
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="blocks-loading"
-      wrapperStyle={{}}
-      wrapperClass="blocks-wrapper"
-      colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-    />
+    <div className={styles.loaderWrap}>
+      <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />
+    </div>
   );
 };
 
